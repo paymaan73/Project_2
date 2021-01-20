e45b01ad986263b6c938636a782aa4b22e65ca8f
paymaan73 <paymankazemipymkz@gmail.com>
2021-01-20 13:47:24 +0330

    update3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
   root 'pages#home'
   resources :users, only:[:index]
-  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
+
+  devise_for :users, path: 'auth', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'secret',
+    confirmation: 'verification',
+    unlock: 'unblock',
+    registration: 'register',
+    sign_up: 'cmon_let_me_in',
+  }
+
+  devise_scope :user do
+    delete 'auth/logout', to: 'devise/sessions#destroy'
+  end
 
   resources :posts
+  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
 end
